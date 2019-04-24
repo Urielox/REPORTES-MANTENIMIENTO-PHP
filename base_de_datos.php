9c5a1de083837884e37c92fb343315ee16163162
Urielox <43377310+Urielox@users.noreply.github.com>
2019-04-23 19:56:24 -0500

    Update base_de_datos.php

--- base_de_datos.php
+++ base_de_datos.php
@@ -1,13 +1,21 @@
 <?php
 $contraseña = "n0m3l0";
 $usuario = "root";
-$nombre_base_de_datos = "ventas";
+$nombre_base_de_datos = "reportes";
+$nombre_base_de_datos2 = "FAQ";
 try{
 	$base_de_datos = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
 	 $base_de_datos->query("set names utf8;");
     $base_de_datos->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
     $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $base_de_datos->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+	
+	$base_de_datos2 = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos2, $usuario, $contraseña);
+	 $base_de_datos2->query("set names utf8;");
+    $base_de_datos2->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+    $base_de_datos2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $base_de_datos2->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+	
 }catch(Exception $e){
 	echo "Ocurrió algo con la base de datos: " . $e->getMessage();
 }
